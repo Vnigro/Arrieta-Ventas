d2bfab03f6bd90656bc7cb588c4c132f9ed30522
Vnigro <nigrovalentin6@gmail.com>
2026-09-07 14:48:43 -0300

    fix: bundle backend serverless via dist output

--- api/index.ts
+++ api/index.ts
@@ -1,28 +1,3 @@
-import express from "express";
-import { createExpressMiddleware } from "@trpc/server/adapters/express";
-import { registerOAuthRoutes } from "../server/_core/oauth.js";
-import { registerStorageProxy } from "../server/_core/storageProxy.js";
-import { appRouter } from "../server/routers.js";
-import { createContext } from "../server/_core/context.js";
-
-const app = express();
-
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ limit: "50mb", extended: true }));
-
-app.get("/api", (_req, res) => {
-  res.send("Backend Serverless funcionando correctamente!");
-});
-
-registerStorageProxy(app);
-registerOAuthRoutes(app);
-
-app.use(
-  ["/api/trpc", "/trpc"],
-  createExpressMiddleware({
-    router: appRouter,
-    createContext,
-  })
-);
+import app from "../dist/index.js";
 
 export default app;
